2e1dbf6f776f15f9f96658873b03cda793c0617d
Andrew <neimanfletche@gmail.com>
2023-06-05 00:12:25 -0400

    fixed dragging bug

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -2,7 +2,7 @@ window.onload = function () {
   const svg = document.querySelector(".worldMap");
   const container = document.querySelector(".container");
   const group = document.getElementById("group");
-  console.log(container);
+
   container.addEventListener("wheel", scaleMap);
 
   function scaleMap(event) {
@@ -15,6 +15,7 @@ window.onload = function () {
     const mouseY = event.clientY - containerRect.top;
     group.style.transformOrigin = `${mouseX}px ${mouseY}px`;
     group.style.transform = `scale(${scaleBy})`;
+    adjustSvgContainerSize();
   }
 
   // Variables to store the initial position and mouse offset
@@ -23,30 +24,50 @@ window.onload = function () {
   let offsetX = 0;
   let offsetY = 0;
   let isDragging = false;
+  let newX = 0;
+  let newY = 0;
+  let stack = new Array();
 
   // Function to handle mouse down event
   const startDrag = (event) => {
     // Store the initial position and calculate the mouse offset
-    initialX = svg.getAttribute("cx");
-    initialY = svg.getAttribute("cy");
-    offsetX = event.clientX - initialX;
-    offsetY = event.clientY - initialY;
+    offsetX = event.clientX;
+    offsetY = event.clientY;
+    if (stack.length != 0) {
+      const arr = stack.pop();
+      initialX = arr[0];
+      initialY = arr[1];
+    }
     isDragging = true;
+    adjustSvgContainerSize();
   };
+
+  //change scroll bar instead of moving the image
 
   // Function to handle mouse move event
   const drag = (event) => {
     if (isDragging) {
       // Update the position of the SVG element
-      const newX = event.clientX - offsetX;
-      const newY = event.clientY - offsetY;
-      svg.setAttribute("transform", `translate(${newX}, ${newY})`);
+      newX = event.clientX - offsetX;
+      newY = event.clientY - offsetY;
+      svg.setAttribute(
+        "transform",
+        `translate(${initialX + newX}, ${initialY + newY})`
+      );
+      adjustSvgContainerSize();
     }
   };
 
   // Function to handle mouse up event
-  const endDrag = () => {
+  const endDrag = (event) => {
+    stack.push([initialX + newX, initialY + newY]);
     isDragging = false;
+  };
+
+  const adjustSvgContainerSize = () => {
+    const bbox = group.getBBox();
+    container.style.width = bbox.width + "px";
+    container.style.height = bbox.height + "px";
   };
 
   // Attach event listeners to the SVG element
